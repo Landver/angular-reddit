f3d80015f1bc7f4f9bece315edbeccc6d0c08992
Landver <good1ni@mail.ru>
2018-08-15 20:30:49 +0300

    page 86

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { Article } from './article.model';
+import { headersToString } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-article',
@@ -25,4 +26,57 @@ export class ArticleComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+
+class Person {
+  first_name: string;
+  last_name: string;
+  age: number;
+
+  constructor(first_name: string, last_name: string, age: number) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log('Hello', this.first_name);
+  }
+
+  ageInYears(years: number): number {
+    return this.age + years;
+  }
+}
+
+
+class Report {
+  data: string[];
+  age: number;
+
+  constructor(data: string[], age: number) {
+    this.data = data;
+    this.age = age;
+  }
+
+  run() {
+    this.data.forEach(
+      function(line) { console.log(line); }
+    );
+    console.log(this.age);
+  }
+}
+
+class TabbedReport extends Report {
+  headers: string[];
+
+  constructor(headers: string[], data: string[], age: number) {
+    super(data, age);
+    this.headers = headers;
+  }
+
+  run() {
+    console.log(this.headers);
+    super.run();
+  }
 }
